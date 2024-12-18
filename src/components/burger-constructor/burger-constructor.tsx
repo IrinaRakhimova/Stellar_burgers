21c3196227ec85c87b4d18fea143a49036fd3ecc
irina <rakhimovairinav@gmail.com>
2024-12-19 01:13:35 +0200

    BurgerCondtructor styles corrected

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -4,6 +4,7 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import { Ingredient } from '../../utils/data';
 import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import classNames from 'classnames';
 
 interface BurgerConstructorProps {
     burgerData: Ingredient[];
@@ -29,57 +30,57 @@ export const BurgerConstructor: React.FC<BurgerConstructorProps> = ({ burgerData
         </section>
         <div className={`${styles.scroll} custom-scroll`}>
             <section className={styles.typeSection}>
-                <h2 className="text text_type_main-medium mt-10">Булки</h2>
+                <h2 className="text text_type_main-medium mt-10 mb-6">Булки</h2>
                 <ul className={styles.ingredientType}>
                     {burgerData.map((ingredient) => ingredient.type === "bun" && (
-                        <li className={styles.ingredient}>
+                        <li className={classNames(styles.ingredient, 'mb-8')}>
                             <img src={ingredient.image} alt={ingredient.name} />
                             <Counter count={1} size="default" extraClass="m-1" />
 
                             <div className={styles.price}>    
-                                <p className='text text_type_digits-default mr-2 mb-2'>{ingredient.price}</p>
+                                <p className='text text_type_digits-default mt-2 mb-2'>{ingredient.price}</p>
                                 <CurrencyIcon type="primary" />
                             </div> 
 
-                            <p className="text text_type_main-default">{ingredient.name}</p>
+                            <p className={classNames(styles.name, "text text_type_main-default")}>{ingredient.name}</p>
                         </li>
                     ))} 
                 </ul>
             </section>
 
             <section className={styles.typeSection}>
-                <h2 className="text text_type_main-medium mt-10">Соусы</h2>
+                <h2 className="text text_type_main-medium mt-2 mb-6">Соусы</h2>
                 <ul className={styles.ingredientType}>
                     {burgerData.map((ingredient) => ingredient.type === "sauce" && (
-                        <li className={styles.ingredient}>
+                        <li className={classNames(styles.ingredient, 'mb-8')}>
                             <img src={ingredient.image} alt={ingredient.name} />
                             <Counter count={1} size="default" extraClass="m-1" />
 
                             <div className={styles.price}>    
-                                <p className='text text_type_digits-default mr-2 mb-2'>{ingredient.price}</p>
+                                <p className='text text_type_digits-default mt-2 mb-2'>{ingredient.price}</p>
                                 <CurrencyIcon type="primary" />
                             </div> 
 
-                            <p className="text text_type_main-default">{ingredient.name}</p>
+                            <p className={classNames(styles.name, "text text_type_main-default")}>{ingredient.name}</p>
                         </li>
                     ))} 
                 </ul>
             </section>
 
             <section className={styles.typeSection}>
-                <h2 className="text text_type_main-medium mt-10">Начинка</h2>
-                <ul className={styles.ingredientType}>
+                <h2 className="text text_type_main-medium mt-2 mb-6">Начинка</h2>
+                <ul className={classNames(styles.ingredientType, 'mb-8')}>
                     {burgerData.map((ingredient) => ingredient.type === "main" && (
                         <li className={styles.ingredient}>
                             <img src={ingredient.image} alt={ingredient.name} />
                             <Counter count={1} size="default" extraClass="m-1" />
 
                             <div className={styles.price}>    
-                                <p className='text text_type_digits-default mr-2 mb-2'>{ingredient.price}</p>
+                                <p className='text text_type_digits-default mt-2 mb-2'>{ingredient.price}</p>
                                 <CurrencyIcon type="primary" />
                             </div> 
 
-                            <p className="text text_type_main-default">{ingredient.name}</p>
+                            <p className={classNames(styles.name, "text text_type_main-default")}>{ingredient.name}</p>
                         </li>
                     ))} 
                 </ul>
